5b8cc39849128e2c7bf2be133c9e9a781e443359
lhchau <lhchau20@apcs.fitus.edu.vn>
2024-08-07 23:24:47 +0700

    update

--- optimizer/__init__.py
+++ optimizer/__init__.py
@@ -1,6 +1,8 @@
 from torch.optim import SGD
 from .sam import SAM
-
+from .samonly import SAMONLY
+from .samwo import SAMWO
+from .samen import SAMEN
 
 def get_optimizer(
     net,
@@ -10,5 +12,11 @@ def get_optimizer(
         return SAM(net.parameters(), **opt_hyperpara)
     elif opt_name == 'sgd':
         return SGD(net.parameters(), **opt_hyperpara)
+    elif opt_name == 'samonly':
+        return SAMONLY(net.parameters(), **opt_hyperpara)
+    elif opt_name == 'samwo':
+        return SAMWO(net.parameters(), **opt_hyperpara)
+    elif opt_name == 'samen':
+        return SAMEN(net.parameters(), **opt_hyperpara)
     else:
         raise ValueError("Invalid optimizer!!!")
