e3ef168d3f6d051e1130ca84c787a6c1822b967c
lhchau <lhchau20@apcs.fitus.edu.vn>
2024-09-01 09:46:08 +0700

    update

--- train.py
+++ train.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
     for epoch in range(start_epoch, start_epoch+EPOCHS):
         print('\nEpoch: %d' % epoch)
         if alpha_scheduler:
-            optimizer.set_alpha(get_alpha(epoch, initial_alpha=1, final_alpha=cfg['optimizer']['condition'], total_epochs=EPOCHS))
+            optimizer.set_alpha(get_alpha(epoch, initial_alpha=1, final_alpha=cfg['optimizer']['condition'], total_epochs=alpha_scheduler))
         loop_one_epoch(
             dataloader=train_dataloader,
             net=net,
--- utils/utils.py
+++ utils/utils.py
@@ -14,7 +14,10 @@ import torch.nn as nn
 import torch.nn.init as init
 
 def get_alpha(epoch, initial_alpha, final_alpha, total_epochs):
-    alpha = initial_alpha - (initial_alpha - final_alpha) * (epoch / total_epochs)
+    if epoch < total_epochs:
+        alpha = initial_alpha - (initial_alpha - final_alpha) * (epoch / total_epochs)
+    else:
+        alpha = final_alpha
     return alpha
 
 def get_gradients(optimizer):
