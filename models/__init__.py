9aa33e85cbc4db67a21e0cca23ba8d01d5ab2503
lhchau <lhchau20@apcs.fitus.edu.vn>
2024-10-24 10:43:18 +0700

    update

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,7 @@ from .densenet import *
 from .inceptionresnetv2 import inceptionresnetv2
 from torchvision.models import efficientnet_b0
 import torchvision
+from .resnetv2 import *
 
 
 def get_model(model_name, num_classes, widen_factor=1):
@@ -36,5 +37,9 @@ def get_model(model_name, num_classes, widen_factor=1):
         return torchvision.models.resnet18(num_classes=num_classes)
     elif model_name == "resnet50_webvision":
         return torchvision.models.resnet50(num_classes=num_classes)
+    elif model_name == "resnet32":
+        return resnet32(num_classes=num_classes)
+    elif model_name == "resnet56":
+        return resnet56(num_classes=num_classes)
     else:
         raise ValueError("Invalid model!!!")
