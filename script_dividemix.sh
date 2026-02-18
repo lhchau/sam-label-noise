30fa4120683b127b41217d24e77e12a5d09f75a7
lhchau <lhchau20@apcs.fitus.edu.vn>
2026-02-18 16:49:48 -0500

    update

---
+++ script_dividemix.sh
@@ -0,0 +1,160 @@
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10_index \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+
+
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+
+
+####
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+
+
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+
+python train_dividemix.py config/train_sam_dividemix.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-dividemix
+  
