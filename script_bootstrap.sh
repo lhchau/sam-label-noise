e719b8672b897b1e680d454c4f6c343f15e4b5d0
lhchau <lhchau20@apcs.fitus.edu.vn>
2026-02-18 17:09:28 -0500

    update

---
+++ script_bootstrap.sh
@@ -0,0 +1,455 @@
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+
+####
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar10 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar10-sam-noise-co-teaching
+  
+
+
+####
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sgd.yaml \
+  --optimizer.opt_name=sgd \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+
+####
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.25 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=42 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=43 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+
+python train_bootstrap.py config/train_sam.yaml \
+  --optimizer.opt_name=sam \
+  --optimizer.rho=0.1 \
+  --optimizer.alpha=0.5 \
+  --trainer.alpha_scheduler=50 \
+  --model.model_name=resnet18 \
+  --model.widen_factor=1 \
+  --dataloader.data_name=cifar100 \
+  --dataloader.noise=0.5 \
+  --dataloader.samples_per_class=None \
+  --trainer.seed=44 \
+  --logging.framework_name=wandb \
+  --logging.project_name=cifar100-sam-noise-co-teaching
+  
